6513abf07bc2c9977d96d5b14aa1202ae9800583
Cristian Mármol <>
2020-02-10 23:26:33 +0100

    feat(): Added Hoc and containers to call gql

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import {ListOfCategories} from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
-import {ListOfPhotoCards} from './components/ListOfPhotoCards'
+import {ListOfPhotoCards} from './container/ListOfPhotoCards'
 import {Logo} from './components/Logo'
-export const App = () => (
-    <div>
-        <GlobalStyle/>
-        <Logo/>
-        <ListOfCategories/>
-        <ListOfPhotoCards/>
-    </div>
-    
-)
+import {PhotoCardWithQuery} from './container/PhotoCardWithQuery'
+export const App = () => {
+    const urlParams = new window.URLSearchParams(window.location.search)
+    const detailId = urlParams.get('detail')
+    return (
+        <div>
+            <GlobalStyle/>
+            <Logo/>
+            {
+                detailId
+                ? <PhotoCardWithQuery id={detailId}/>
+                : <React.Fragment>
+                    <ListOfCategories/>
+                    <ListOfPhotoCards categoryId={'2'}/>
+                </React.Fragment>
+            }
+            
+        </div>
+    )
+}
--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,22 +1,8 @@
 import React from 'react'
 import {PhotoCard} from '../PhotoCard'
-import {graphql} from 'react-apollo'
-import {gql} from 'apollo-boost'
 
-const withPhotos = graphql(gql`
-query getPhotos{
-    photos{
-      id
-      categoryId
-      src
-      likes
-      liked
-      userId
-    }
-  }
-`)
 
-const ListOfPhotoCardsComponent = ({data: {photos = []}}) => {
+export const ListOfPhotoCardsComponent = ({data: {photos = []}}) => {
     console.log({photos})
     return (
         <ul>
@@ -28,4 +14,3 @@ const ListOfPhotoCardsComponent = ({data: {photos = []}}) => {
     )
 }
 
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
